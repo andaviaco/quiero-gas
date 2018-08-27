3f818cb2cb5acc32e9fa8c5ec826ad247486835e
Andrés Ávila <andaviaco@gmail.com>
2018-08-26 19:07:03 -0500

    Add Map view and test button

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { MapView } from 'expo';
 
 export default class App extends React.Component {
+  handleBestDealPress() {
+    console.log('Kaka');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+
+        <Button
+          onPress={this.handleBestDealPress}
+          title="Mejor Precio"
+          color="#841584"
+          accessibilityLabel="Mostrar estación con mejor precio"
+        />
       </View>
     );
   }
@@ -19,5 +37,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
